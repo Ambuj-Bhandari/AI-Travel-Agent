7eed24477a969c281ce2c25d0a2061f9b6749e3b
Ambuj Bhandari <ambujbhandari546@gmail.com>
2025-09-24 15:57:08 +0530

    Project Setup,Logging and Exception

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
+
+setup(
+    name = "AI Travel Agent",
+    version="0.1",
+    author="Ambuj Bhandari",
+    packages= find_packages(),
+    install_requires = requirements
+)
